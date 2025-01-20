7c33a7a4f05b055c9d4850cb1c6e5a6e70f82fa8
naveen27072004 <naveenlatha2772004@gmail.com>
2025-01-20 17:17:21 +0530

    metrial ui importaed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <>
-      <h1 className='text-3xl bg-slate-500'>Hello world</h1>
+    <Home/>
     </>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <div className='min-h-screen bg-green-400'>
+        <div>
+          <div>
+
+          </div>
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
